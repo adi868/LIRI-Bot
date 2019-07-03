fb1aa599854958698bac35aaff09c47ab4f80e9e
alciancio <alcianciotto@gmail.com>
2019-07-03 11:40:18 -0500

    chalk and other require commands

--- liri.js
+++ liri.js
@@ -1,20 +1,31 @@
+//read and set any environmental variables with dotenv package
+//requires .env file
+require("dotenv").config();
+
 //npm install inquirer
 var inquirer = require("inquirer");
-//read and set any environmental variables with dotenv package
-require("dotenv").config();
-//import js file and store in variable 
+//install axios
+var axios = require("axios");
+//install moment
+var moment = require("moment")
+//install spotify API
+var Spotify= require("node-spotify-api");
+var fs = require("fs")
+//install chalk
+const chalk = require('chalk');
+
+//import js file and store in variable. require keys file for API keys
 var keys = require("./keys.js")
-//access spotify keys information
+
+//access Spotify keys information
 var spotify = new Spotify(keys.spotify)
-
-
 
 
 inquirer
     .prompt([
         {
             type: "list",
-            message: "Welcome to LIRI-Bot!" + "\n" + "Which command do you want to run?",
+            message: (chalk.blue("Welcome to LIRI-Bot!")) + "\n" + "Which command do you want to run?",
             name: "command",
             choices: ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says']
         },
